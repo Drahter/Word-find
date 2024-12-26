6565f4658e8cabca86c529d0a246612f40cd267f
Dmit Bashmakov <denegniy1@gmail.com>
2024-12-26 14:57:17 +0500

    env file settings

--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 # from datetime import timedelta
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,14 +127,7 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
-}
+#
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
