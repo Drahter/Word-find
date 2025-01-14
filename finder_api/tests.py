19b7f90c21c364677c00c9ca40c4c71434e6ac26
Dmit Bashmakov <denegniy1@gmail.com>
2025-01-14 14:00:21 +0500

    added tests for API functions

--- finder_api/tests.py
+++ finder_api/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from finder.models import Article
+from finder.search import ArticleIndex
+
+
+class ArticleAPITestCase(APITestCase):
+    def setUp(self):
+        self.article = Article.objects.create(rubrics=['Тестовая', 'статья'], text='Текст для тестовой статьи')
+
+    def test_article_retrieve(self):
+        url = reverse('finder_api:api_article_detail', args=(self.article.pk,))
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+            data.get('rubrics'), ['Тестовая', 'статья']
+        )
+        self.assertEqual(
+            data.get('id'), 1
+        )
+
+    def test_elasticsearch_api_get_results(self):
+        url = reverse('api_search')
+        response = self.client.post(url, {'query': 'текст'}, format='json')
+        results = response.json
+
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(len(results), 1)
+
+        self.assertEqual(
+            results[0]['rubrics'], 'Тестовая, статья'
+        )
+
+    def clear(self):
+        ArticleIndex.get(id=self.article.pk).delete()
+        print("Document deleted")  # Логирование удаления
